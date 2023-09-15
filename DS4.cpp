9a316ee68257a748387738d8ff66a4e00a5fc888
MRJ-XZ <mohammad.mrjr@gmail.com>
2023-09-15 13:24:11 +0330

    -added lightbar and vibration demo -changes in README

--- DS4.cpp
+++ DS4.cpp
@@ -10,20 +10,61 @@
 #include <stdlib.h>
 #include <string>
 
-char buffer[256] = {0};
-char command[29] = "sudo chmod a+rw /dev/hidraw0";
+char buffer[256] = {0}, command[29] = "sudo chmod a+rw /dev/hidraw0";
+unsigned char output[9] = {0};
+std::string name = "Sony Interactive Entertainment Wireless Controller",
+    data = "report",
+    light = "light",
+    shock = "shock";
+int fd = 0 , desc_size;
+
+void get_data(){
+    read(fd , buffer , 64);
+    for(int i=0 ; i < 10;i++){
+        printf("%hhx\t" , buffer[i]);
+        if((i) % 10 == 9)
+            printf("\n");
+    }
+}
+void lightbar_demo(){
+    for (int i = 0; i < 256; i++)
+    {
+        output[7] = (char)(255 - i);
+        output[8] = (char)i;
+        write(fd , output , 9);
+        usleep(10000);
+    }
+    for (int i = 0; i < 256; i++)
+    {
+        output[7] = (char)i;
+        output[8] = (char)(255 - i);
+        write(fd , output , 9);
+        usleep(10000);
+    }
+}
+void shock_demo(){
+    for(int i=0;i < 200;i++){
+        output[5] = (char)i;
+        write(fd , output , 9);
+        usleep(20000);
+    }
+    for(int i=200;i >= 0; i--){
+        output[5] = (char)i;
+        write(fd , output , 9);
+        usleep(20000);        
+    }
+    sleep(2);
+}
 int main(int argc, char** argv){
     struct hidraw_report_descriptor rpt_desc;
     struct hidraw_devinfo info;
     struct stat status;
-    int fd = 0 , desc_size;
     
     for(int i=1;;i++){
         if(stat(&command[16] , &status) == 0){
             system(command);
             fd = open(&command[16] ,  O_RDWR | O_NONBLOCK);
             ioctl(fd , HIDIOCGRAWNAME(256) , buffer);
-            std::string name = "Sony Interactive Entertainment Wireless Controller";
             if(name.compare(buffer) == 0){
                 // printf("%d\n" , desc_size);
                 // rpt_desc.size = desc_size;
@@ -52,13 +93,25 @@ int main(int argc, char** argv){
             return -1;            
         }
     }
-    while(true){
-        usleep(4000);
-        read(fd , buffer , 64);
-        for(int i=0 ; i < 10;i++){
-            printf("%hhx\t" , buffer[i]);
-            if((i) % 10 == 9)
-                printf("\n");
+    output[0] = 0x5;
+    output[6] = 0x0;
+    output[7] = 0x0;
+    output[8] = 0x0;
+    if(data.compare(argv[1]) == 0){
+        while(true){
+            get_data();
+            usleep(4000);
         }
+    }
+    else if(light.compare(argv[1]) == 0){
+        output[1] = 0x2;
+        while(true)
+            lightbar_demo();
+    }
+    else if(shock.compare(argv[1]) == 0){
+        output[1] = 0x1;
+        output[4] = 0xC0;   
+        while(true)
+            shock_demo();
     }
 }
